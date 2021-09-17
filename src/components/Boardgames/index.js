0335d41793c232e68e9a9da7cd08f84a1b9accda
Flo Nz <florian.nunez@gmail.com>
2021-09-17 18:37:52 +0200

    présentation jeux en cards

--- src/components/Boardgames/index.js
+++ src/components/Boardgames/index.js
@@ -30,21 +30,61 @@ const Boardgames = ({ boardgames, categories }) => {
           ))
         }
       </div>
-      <div className="boardgames-list">
-        <Card.Group itemsPerRow={4}>
+      <div className="boardgames-list" id="cards">
         {
           boardgames.map((bg) => (
-            <Card
-            key={bg.id}
-            color='green'
-            image={bg.picture_url}
-            header={bg.name}
-            meta={bg.categories[0]['name']}
-            description={"Joueurs : " + bg.min_players + "-" + bg.max_players + " / Durée : " + bg.duration + " minutes"}
-          />
+            
+            <figure key={bg.id} className={'card' + ` card--${bg.difficulty[0].label}`}>
+              <div className="card__image-container">
+                <img src={bg.picture_url} alt={bg.name} className="card__image" />   
+              </div>
+              
+              <figcaption className="card__caption">
+                <h1 className="card__name">{bg.name}</h1>
+
+                <h3 className="card__type">
+                  {bg.difficulty[0].label}
+                </h3>
+
+                <table className="card__stats">
+                  <tbody><tr>
+                    <th>Joueurs min</th>
+                    <td>{bg.min_players}</td>
+                  </tr>
+                  <tr>
+                    <th>Joueurs max</th>
+                    <td>{bg.max_players}</td>
+                  </tr>
+                  
+                  <tr>
+                    <th>Durée (minutes)</th>
+                    <td>{bg.duration}</td>
+                  </tr>
+
+                  <tr>
+                    { bg.themes.length > 1 && <th>Thèmes</th> }
+                    { bg.themes.length <= 1 && <th>Thème</th>}
+                    {
+                      bg.themes.map((theme) => (
+                        <td key={theme.id}>{theme.name}</td>
+                      ))
+                    }
+                  </tr>
+                </tbody></table>
+                
+                <div className="card__abilities">
+                {
+                  bg.categories.map((cat) => (
+                    <h4 key={cat.id} className="card__ability">
+                      <span className="card__label">{cat.name}</span>
+                    </h4> 
+                  ))
+                }
+                </div>
+              </figcaption>
+            </figure>
           ))
         }
-        </Card.Group>
       </div>
     </main>
   )
